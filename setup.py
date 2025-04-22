17a039707c69c97ca6b47847d59e55a64330e25e
Aditya Santosh Aher <129608959+asaher84@users.noreply.github.com>
2025-04-22 10:18:58 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Cryptocurrency Liquidity Prediction for Market Stability",
+    version="0.1",
+    author="Aditya Aher",
+    author_email="adiaher84@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
